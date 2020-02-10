47080166a5292962e697fb1a24d8dc95cbc328f1
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-10 20:17:32 +0000

    m690-user-9-PKQ1.190118.001-6-release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- lineage_Starlord.mk
+++ lineage_Starlord.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := Nokia 6.2
 PRODUCT_GMS_CLIENTID_BASE := android-nokia
 TARGET_VENDOR := nokia
 TARGET_VENDOR_PRODUCT_NAME := Starlord
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="m690-user 9 PKQ1.190118.001 76 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="m690-user 9 PKQ1.190118.001 6 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Nokia/Starlord_00WW/SLD_sprout:9/PKQ1.190118.001/00WW_1_160:user/release-keys
+BUILD_FINGERPRINT := Nokia/Starlord_00WW/SLD_sprout:9/PKQ1.190118.001/00WW_1_170:user/release-keys
